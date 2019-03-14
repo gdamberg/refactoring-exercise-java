91d90eeeeb1388f57edfb8292fd6425057079c8f
Göran Damberg <gdamberg@outlook.com>
2019-03-14 19:40:19 +0100

    Test cases

--- test/com/greatersum/rental/RentalTests.java
+++ test/com/greatersum/rental/RentalTests.java
@@ -1,16 +1,101 @@
 package com.greatersum.rental;
 
 import org.approvaltests.Approvals;
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Arrays;
 
 public class RentalTests {
+    private RentalInfo info = new RentalInfo();
+
     @Test
-    public void MartinTest() {
-        Customer customer = new Customer("martin", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1)));
-        RentalInfo info = new RentalInfo();
+    public void RentRegularMovieFor1Day() {
+        Customer customer = new Customer("martin", Arrays.asList(new MovieRental("F001", 1)));
+        String actualResult = info.statement(customer);
+        Approvals.verify(actualResult);
+    }
+
+    @Test
+    public void RentRegularMovieFor2Day() {
+        Customer customer = new Customer("martin", Arrays.asList(new MovieRental("F001", 2)));
+        String actualResult = info.statement(customer);
+        Approvals.verify(actualResult);
+    }
+
+    @Test
+    public void RentRegularMovieFor7Day() {
+        Customer customer = new Customer("martin", Arrays.asList(new MovieRental("F001", 7)));
+        String actualResult = info.statement(customer);
+        Approvals.verify(actualResult);
+    }
+
+    @Test
+    public void RentChildrensMovieFor1Day() {
+        Customer customer = new Customer("martin", Arrays.asList(new MovieRental("F003", 1)));
+        String actualResult = info.statement(customer);
+        Approvals.verify(actualResult);
+    }
+
+    @Test
+    public void RentChildrensMovieFor2Day() {
+        Customer customer = new Customer("martin", Arrays.asList(new MovieRental("F003", 2)));
+        String actualResult = info.statement(customer);
+        Approvals.verify(actualResult);
+    }
+
+    @Test
+    public void RentChildrensMovieFor7Day() {
+        Customer customer = new Customer("martin", Arrays.asList(new MovieRental("F003", 7)));
+        String actualResult = info.statement(customer);
+        Approvals.verify(actualResult);
+    }
+
+    @Test
+    public void RentNewMovieFor1Day() {
+        Customer customer = new Customer("martin", Arrays.asList(new MovieRental("F004", 1)));
+        String actualResult = info.statement(customer);
+        Approvals.verify(actualResult);
+    }
+
+    @Test
+    public void RentNewMovieFor2Day() {
+        Customer customer = new Customer("martin", Arrays.asList(new MovieRental("F004", 2)));
+        String actualResult = info.statement(customer);
+        Approvals.verify(actualResult);
+    }
+
+    @Test
+    public void RentNewMovieFor7Day() {
+        Customer customer = new Customer("martin", Arrays.asList(new MovieRental("F004", 7)));
+        String actualResult = info.statement(customer);
+        Approvals.verify(actualResult);
+    }
+    @Test
+    public void Rent2Movies2Days() {
+        Customer customer = new Customer("martin", Arrays.asList(
+                new MovieRental("F001", 2),
+                new MovieRental("F002", 2)));
+        String actualResult = info.statement(customer);
+        Approvals.verify(actualResult);
+    }
+
+    @Test
+    public void Rent3Movies3Days() {
+        Customer customer = new Customer("martin", Arrays.asList(
+                new MovieRental("F001", 3),
+                new MovieRental("F002", 3),
+                new MovieRental("F003", 3)));
+        String actualResult = info.statement(customer);
+        Approvals.verify(actualResult);
+    }
+
+    @Test
+    public void Rent4Movies4Days() {
+        Customer customer = new Customer("martin", Arrays.asList(
+                new MovieRental("F001", 4),
+                new MovieRental("F002", 4),
+                new MovieRental("F003", 4),
+                new MovieRental("F004", 4)));
         String actualResult = info.statement(customer);
         Approvals.verify(actualResult);
     }

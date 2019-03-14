0be8996f358fd2b392b2675736b624e3cdce6e18
Göran Damberg <gdamberg@outlook.com>
2019-03-14 20:01:58 +0100

    Round 1. Breaking up statement function in smaller pieces

--- src/com/greatersum/rental/RentalInfo.java
+++ src/com/greatersum/rental/RentalInfo.java
@@ -1,14 +1,12 @@
 package com.greatersum.rental;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 
 public class RentalInfo {
 
     public String statement(Customer customer) {
-        HashMap<String, Movie> movies = new HashMap();
+        HashMap<String, Movie> movies = new HashMap<>();
         movies.put("F001", new Movie("Ran", "regular"));
         movies.put("F002", new Movie("Trois Couleurs: Bleu", "regular"));
         movies.put("F003", new Movie("Cars 2", "childrens"));
@@ -17,33 +15,12 @@ public class RentalInfo {
         BigDecimal totalAmount = BigDecimal.valueOf(0);
         int frequentRenterPoints = 0;
         String result = "Rental Record for " + customer.getName() + "\n";
+
         for (MovieRental r : customer.getRentals()) {
             Movie movie = movies.get(r.getMovieId());
-            BigDecimal thisAmount = BigDecimal.valueOf(0);
 
-            // determine amount for each movie
-            switch (movie.getCode()) {
-                case "regular":
-                    thisAmount = BigDecimal.valueOf(2);
-                    if (r.getDays() > 2) {
-                        thisAmount = BigDecimal.valueOf((r.getDays() - 2) * 1.5).add(thisAmount);
-                    }
-                    break;
-                case "new":
-                    thisAmount = BigDecimal.valueOf(r.getDays() * 3);
-                    break;
-                case "childrens":
-                    thisAmount = BigDecimal.valueOf(1.5);
-                    if (r.getDays() > 3) {
-                        thisAmount = BigDecimal.valueOf((r.getDays() - 3) * 1.5).add(thisAmount);
-                    }
-                    break;
-            }
-
-            //add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if (movie.getCode() == "new" && r.getDays() > 2) frequentRenterPoints++;
+            BigDecimal thisAmount = calculateMoviePrice(r, movie);
+            frequentRenterPoints += calculateFrequentRentalPoints(r, movie);
 
             //print figures for this rental
             result += "\t" + movie.getTitle() + "\t" + thisAmount + "\n";
@@ -54,5 +31,36 @@ public class RentalInfo {
         result += "You earned " + frequentRenterPoints + " frequent renter points\n";
 
         return result;
+    }
+
+    private int calculateFrequentRentalPoints(MovieRental r, Movie movie) {
+        int points = 1;
+        // add bonus for a two day new release rental
+        if (movie.getCode().equals("new") && r.getDays() > 2) points++;
+        return points;
+    }
+
+    private BigDecimal calculateMoviePrice(MovieRental r, Movie movie) {
+        BigDecimal price = BigDecimal.valueOf(0);
+
+        // determine amount for each movie
+        switch (movie.getCode()) {
+            case "regular":
+                price = BigDecimal.valueOf(2);
+                if (r.getDays() > 2) {
+                    price = BigDecimal.valueOf((r.getDays() - 2) * 1.5).add(price);
+                }
+                break;
+            case "new":
+                price = BigDecimal.valueOf(r.getDays() * 3);
+                break;
+            case "childrens":
+                price = BigDecimal.valueOf(1.5);
+                if (r.getDays() > 3) {
+                    price = BigDecimal.valueOf((r.getDays() - 3) * 1.5).add(price);
+                }
+                break;
+        }
+        return price;
     }
 }
